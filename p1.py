3903e1a5ebee93ab4db4d48cb44c5380904189af
AaratiS12 <aarati.srikumar@gmail.com>
2020-09-18 22:57:20 +0000

    updated spoonacular code that works with image

--- p1.py
+++ p1.py
@@ -8,6 +8,7 @@ from collections import Counter
 import sys
 import random
 import requests
+import json
 
 app = flask.Flask(__name__)
 
@@ -23,27 +24,21 @@ def index():
     auth.set_access_token(access_token, access_token_secret)
     auth_api = API(auth)
     
-    food_items = ["ramen", "dosas", "french baguette", "dumplings", "pesto pasta", "paneer", "rice"]
+    food_items = ["ramen", "dosas", "french baguette", "dumpling", "pesto pasta", "paneer", "rice"]
     random_num = random.randint(0, len(food_items) - 1)
-    
-    for tweet in auth_api.search(q=food_items[random_num], lang="en", result_type="recent"):
+    print(random_num)
+    for tweet in auth_api.search(q=food_items[random_num], lang="en", result_type="popular"):
         user = tweet.user.name
         tweets = tweet.text
         
     spoonacular_key = os.environ['spoonacular_auth_key']  
-    
-    url = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + spoonacular_key+"&query=" + food_items[random_num]
-    
-    #x = "https://api.spoonacular.com/recipes/716429/information?apiKey=" + spoonacular_key +"&includeNutrition=true"
+    url = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + spoonacular_key+"&query=" + food_items[random_num] +"&number=1"
     response = requests.get(url)
-
-    # Inspect some attributes of the `requests` repository
     json_response = response.json()
     print(json_response)
-
-    return flask.render_template("index.html", u = user, t = tweets)
-  
- 
+    
+    image_food = json_response['results'][0]['image']
+    return flask.render_template("index.html", u = user, t = tweets, image= image_food)
 
 app.run(
     port=int(os.getenv('PORT', 8080)), 
