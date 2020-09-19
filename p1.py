7945968e927abc44fa6df923161c5e01487df74b
AaratiS12 <aarati.srikumar@gmail.com>
2020-09-19 23:00:58 +0000

    Final version MVP for M1 Project 1

--- p1.py
+++ p1.py
@@ -24,21 +24,23 @@ def index():
     auth.set_access_token(access_token, access_token_secret)
     auth_api = API(auth)
     
-    food_items = ["ramen", "dosas", "french baguette", "dumpling", "pesto pasta", "paneer", "rice"]
+    food_items = ["ramen", "dosa", "burrito", "dumplings", "pesto pasta", "paneer", "quesadillas"]
     random_num = random.randint(0, len(food_items) - 1)
-    print(random_num)
-    for tweet in auth_api.search(q=food_items[random_num], lang="en", result_type="popular"):
+    item = food_items[random_num]
+   
+    for tweet in auth_api.search(q=item, lang="en", result_type="popular", count = 1):
         user = tweet.user.name
         tweets = tweet.text
-        
+    print(tweet)
     spoonacular_key = os.environ['spoonacular_auth_key']  
-    url = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + spoonacular_key+"&query=" + food_items[random_num] +"&number=1"
+    url = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + spoonacular_key+"&query=" + item +"&number=1"
     response = requests.get(url)
     json_response = response.json()
     print(json_response)
     
-    image_food = json_response['results'][0]['image']
-    return flask.render_template("index.html", u = user, t = tweets, image= image_food)
+    #image_food = json_response['results'][0]['image']
+    image_food = "https://spoonacular.com/recipeImages/636830-312x231.jpg"
+    return flask.render_template("index.html", u = user, t = tweets, image= image_food, item = item)
 
 app.run(
     port=int(os.getenv('PORT', 8080)), 
