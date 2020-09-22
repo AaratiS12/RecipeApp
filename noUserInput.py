b0cedfabe4fcc7c394f8d84888092fc2c72f3681
AaratiS12 <aarati.srikumar@gmail.com>
2020-09-22 14:19:31 +0000

    Separate userInput and not userInput python files with associated html and css files

---
+++ noUserInput.py
@@ -0,0 +1,53 @@
+import flask
+import os
+from tweepy import OAuthHandler
+from tweepy import API
+from tweepy import Cursor
+from datetime import datetime, date, time, timedelta
+from collections import Counter
+import sys
+import random
+import requests
+import json
+from flask import Flask, render_template, request
+
+app = flask.Flask(__name__)
+
+@app.route('/') #Python decorater
+def index():
+    consumer_key= os.environ['consumer_key']
+    consumer_secret= os.environ['consumer_secret']
+    access_token= os.environ['access_token']
+    access_token_secret= os.environ['access_token_secret']
+    
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    auth_api = API(auth)
+    
+    food_items = ["ramen", "spaghetti", "burrito", "dumplings", "pesto pasta", "paneer", "quesadillas"]
+    random_num = random.randint(0, len(food_items) - 1)
+    item = food_items[random_num]
+    
+    
+    for tweet in auth_api.search(q=item, lang="en", result_type="popular", count = 1):
+        author = tweet.user.name
+        tweets = tweet.text
+        date = tweet.created_at
+    
+    spoonacular_key = os.environ['spoonacular_auth_key']  
+    url = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + spoonacular_key+"&query=" + item +"&number=1"
+    response = requests.get(url)
+    json_response = response.json()
+    d = date.date().strftime('%A %d %B %Y')
+    time = date.strftime("%I:%M %p")
+    
+    image_food = json_response['results'][0]['image']
+    #image_food = "https://spoonacular.com/recipeImages/636830-312x231.jpg"
+    return flask.render_template("index.html", author = author, tweet = tweets, date = d, time = time, image= image_food, item = item)
+    
+    
+    
+app.run(
+    port=int(os.getenv('PORT', 8080)), 
+    host=os.getenv('IP', '0.0.0.0')
+)
