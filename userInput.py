0c64d16d352a3e1972a1221c53f1b4df9fea0e0b
AaratiS12 <aarati.srikumar@gmail.com>
2020-09-29 04:41:51 +0000

    Updated UI and code modularity of userInput

--- userInput.py
+++ userInput.py
@@ -2,9 +2,7 @@ import flask
 import os
 from tweepy import OAuthHandler
 from tweepy import API
-from tweepy import Cursor
 from datetime import datetime, date, time, timedelta
-from collections import Counter
 import sys
 import random
 import requests
@@ -13,43 +11,77 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = flask.Flask(__name__)
 
+def twitter(consumer_key, consumer_secret, access_token, access_token_secret, item):
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    auth_api = API(auth)
+    return(auth_api.search(q=item, lang="en", result_type="popular"))
+    
+    
+def spoonacular(spoonacular_key, item):
+    url_image = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + spoonacular_key+"&query=" + item
+    response = requests.get(url_image)
+    return(response.json())
+    
 @app.route('/') #Python decorater
 def index():
     return flask.render_template("input.html")
     
-   
+  
 @app.route('/', methods=['POST'])
 def my_form_post():
-    
+    '''Environmental variables'''
     consumer_key= os.environ['consumer_key']
     consumer_secret= os.environ['consumer_secret']
     access_token= os.environ['access_token']
     access_token_secret= os.environ['access_token_secret']
+    spoonacular_key = os.environ['spoonacular_auth_key']  
     
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    auth_api = API(auth)
     
-    food_items = ["ramen", "dosa", "burrito", "dumplings", "pesto pasta", "paneer", "quesadillas"]
-    random_num = random.randint(0, len(food_items) - 1)
-    #item = food_items[random_num]
+    '''Random Item'''
     item = request.form['text']
+    print(item)
     
-    for tweet in auth_api.search(q=item, lang="en", result_type="popular", count = 1):
+    
+    '''Processing for Twitter'''
+    search = twitter(consumer_key, consumer_secret, access_token,access_token_secret, item)
+    random_num_tweet = random.randint(0, len(search)-1)
+    if search:
+        tweet = search[random_num_tweet] 
         author = tweet.user.name
         tweets = tweet.text
         date = tweet.created_at
+        d = date.date().strftime('%A %d %B %Y')
+        time = date.strftime("%I:%M %p")
+    else:
+        author = "no author"
+        tweets = "Tweet Error- no tweet found"
+        d = "no date" 
+        time = "no time"
     
-    spoonacular_key = os.environ['spoonacular_auth_key']  
-    url = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + spoonacular_key+"&query=" + item +"&number=1"
-    response = requests.get(url)
-    json_response = response.json()
-    d = date.date().strftime('%A %d %B %Y')
-    time = date.strftime("%I:%M %p")
     
-    image_food = json_response['results'][0]['image']
-    #image_food = "https://spoonacular.com/recipeImages/636830-312x231.jpg"
-    return flask.render_template("main_index.html", author = author, tweet = tweets, date = d, time = time, image= image_food, item = item)
+    '''Processing for Spoonacular'''
+    json_response = spoonacular(spoonacular_key, item)
+    random_num_recipe = random.randint(0, len(json_response)-1)
+    recipe_id = json_response['results'][random_num_recipe]['id']
+    image_food = json_response['results'][random_num_recipe]['image']
+    recipe_title = json_response['results'][random_num_recipe]['title']
+    url_image_ingredients = "https://api.spoonacular.com/recipes/"+ str(recipe_id)+ "/information?apiKey=" + spoonacular_key
+    response_ingredients = requests.get(url_image_ingredients)
+    json_response_ingredient = response_ingredients.json()
+    servings = json_response_ingredient["servings"]
+    prep_time = json_response_ingredient["readyInMinutes"]
+    src_url = json_response_ingredient["sourceUrl"]
+    ingredient_list = []
+    for ingredient in json_response_ingredient["extendedIngredients"]:
+        ingredient_list.append(ingredient["original"])
+    
+    
+    '''Sending data to HTML'''
+    return flask.render_template(
+        "main_index.html", author = author, tweet = tweets, date = d, time = time, image= image_food,
+        recipe_title = recipe_title, servings= servings, prep_time = prep_time, 
+        src_url=src_url, ingredient_list = ingredient_list)
     
 
 app.run(
